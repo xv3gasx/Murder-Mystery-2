590efcc2094e04596be09bf75ba279ef39d18885
x.v3gas.x <old.d4rk.com@gmail.com>
2025-12-13 09:33:37 +0400

    Fix typo in kick message for LocalPlayer

--- Open-Source.lua
+++ Open-Source.lua
@@ -2,5 +2,5 @@ local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
 if LocalPlayer then
-    LocalPlayer:Kick("Script is currentlit disabled for updating. Come Discord for more infirmation. discord.gg/foxname")
+    LocalPlayer:Kick("Script is currently disabled for updating. Come Discord for more infirmation. discord.gg/foxname")
 end
