b40c811d8c559fa49fc7fb598e4ceeaf497374e6
x.v3gas.x <old.d4rk.com@gmail.com>
2026-08-06 23:41:42 +0400

    Update Release.lua

--- Release.lua
+++ Release.lua
@@ -1,19 +1,3 @@
-local StarterGui = game:GetService("StarterGui")
-
-local message = "Foxname Murder Mystery 2 script has been shut down because of Luraph deobfuscation cases. We will be back when we are safe again. For more information: discord.gg/v8ZPq4y2nD"
-
-for _ = 1, 10 do
-    local success = pcall(function()
-        StarterGui:SetCore("SendNotification", {
-            Title = "Foxname Murder Mystery 2",
-            Text = message,
-            Duration = 30
-        })
-    end)
-
-    if success then
-        break
-    end
-
-    task.wait(1)
-end
+game:GetService("Players").LocalPlayer:Kick(
+    "Foxname Murder Mystery 2 script has been shut down because of Luraph deobfuscation cases. We will be back when we are safe again. For more info: discord.gg/v8ZPq4y2nD"
+)
