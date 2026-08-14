59ee7c2caf67e2e3217231bfdff4c10b37147728
x.v3gas.x <old.d4rk.com@gmail.com>
2026-08-14 18:05:09 +0400

    Update Release.lua

--- Release.lua
+++ Release.lua
@@ -1,2 +1,5 @@
 setclipboard("https://discord.gg/v8ZPq4y2nD")
-game:GetService("Players").LocalPlayer:Kick("Foxname Murder Mystery 2 script down because of Luraph obfuscator issues and it wont be up again until Luraph become more safer. For more information: discord.gg/v8ZPq4y2nD (copied into your clipboard)")
+
+game:GetService("Players").LocalPlayer:Kick(
+    "Foxname MM2 is currently down due to Luraph issues. More info: discord.gg/v8ZPq4y2nD"
+)
