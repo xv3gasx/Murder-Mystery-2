1646d067bb1fd9912de6881cc1a8c127144a7f94
x.v3gas.x <old.d4rk.com@gmail.com>
2026-08-14 17:30:11 +0400

    Update Release.lua

--- Release.lua
+++ Release.lua
@@ -1,2 +1,2 @@
 setclipboard("https://discord.gg/v8ZPq4y2nD")
-game:GetService("Players").LocalPlayer:Kick("Foxname Murder Mystery 2 script down because of Luraph obfuscator issues and it wont be up again until Luraph become more safer. For more information: discord.gg/v8ZPq4y2nD")
+game:GetService("Players").LocalPlayer:Kick("Foxname Murder Mystery 2 script down because of Luraph obfuscator issues and it wont be up again until Luraph become more safer. For more information: discord.gg/v8ZPq4y2nD (copied into your clipboard)")
